56c462aa5d999c988cd39aa598beea6d846735a7
Rupi-2000 <r.parzmair@gmail.com>
2025-12-31 13:42:52 +0100

    update train_yolo.py

--- pipe_B_yolo/train_yolo.py
+++ pipe_B_yolo/train_yolo.py
@@ -1,17 +1,24 @@
 from ultralytics import YOLO
 from pathlib import Path
+import torch
+
+# -----------------------------
+# GPU Check
+# -----------------------------
+assert torch.cuda.is_available(), "CUDA nicht verfügbar – NVIDIA Treiber prüfen!"
+print("GPU:", torch.cuda.get_device_name(0))
 
 # -----------------------------
 # Konfiguration
 # -----------------------------
-DATA_YAML = r"C:/Users/rparz/Documents/GitHub/ML_Project/data/yolo_dataset/data.yaml"
-MODEL_WEIGHTS = "yolov8m.pt"   # Pretrained weights
-IMG_SIZE = 1024
-EPOCHS = 50
-BATCH = 8
-DEVICE = "cpu"                  # 0 = erste GPU, "cpu" für CPU
+DATA_YAML = r"./data/yolo_dataset/data.yaml"
+MODEL_WEIGHTS = "yolov8n.pt"   # Pretrained weights
+IMG_SIZE = 640
+EPOCHS = 20
+BATCH = 16
+DEVICE = 0                  # 0 = erste GPU, "cpu" für CPU
 PROJECT_DIR = "runs/layout"
-RUN_NAME = "doclaynet_yolov8m"
+RUN_NAME = "doclaynet_yolov8n"
 
 # -----------------------------
 # Sicherheitschecks
